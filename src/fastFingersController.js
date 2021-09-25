84f49f0ae92f34a4f8ef10b046f688d6289b52c1
koraytugay <ktugay@sonatype.com>
2021-09-24 20:42:08 -0400

    no need for iife

--- src/fastFingersController.js
+++ src/fastFingersController.js
@@ -1,103 +1,101 @@
 import fastFingersService from './fastFingersService.js';
 
-(function gameController() {
-  // Helper functions
-  const byId = id => document.querySelector(`#${id}`);
-  const baseUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/`;
+// Helper functions
+const byId = id => document.querySelector(`#${id}`);
+const baseUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/`;
 
-  const oddImageDiv = byId("pokemon-image-odd-index");
-  const evenImageDiv = byId("pokemon-image-even-index");
-  const userInput = byId("user-input");
-  const remainingTime = byId("remaining-time");
-  const currentWord = byId("current-word");
-  const nextWord = byId("next-word");
+const oddImageDiv = byId("pokemon-image-odd-index");
+const evenImageDiv = byId("pokemon-image-even-index");
+const userInput = byId("user-input");
+const remainingTime = byId("remaining-time");
+const currentWord = byId("current-word");
+const nextWord = byId("next-word");
 
-  // Where we keep the game state
-  let fastFingersGame;
+// Where we keep the game state
+let fastFingersGame;
 
-  // New Game event listener
-  function newGame() {
-    fastFingersGame = fastFingersService.newFastFingersGame();
+// New Game event listener
+function newGame() {
+  fastFingersGame = fastFingersService.newFastFingersGame();
 
-    const pokemonImg = document.createElement("img");
-    pokemonImg.src = `${baseUrl}${fastFingersService.getCurrentId(fastFingersGame)}.png`;
+  const pokemonImg = document.createElement("img");
+  pokemonImg.src = `${baseUrl}${fastFingersService.getCurrentId(fastFingersGame)}.png`;
+  evenImageDiv.innerHTML = '';
+  evenImageDiv.appendChild(pokemonImg);
+
+  userInput.hidden = false;
+  userInput.disabled = false;
+  userInput.value = '';
+  userInput.focus();
+  fetchNextImageShowCurrentImage();
+  redraw();
+}
+
+byId("new-game-button").addEventListener('click', newGame);
+
+setInterval(function() {
+  remainingTime.innerHTML = '';
+  remainingTime.textContent = fastFingersGame.remainingTime;
+  if (fastFingersGame.remainingTime === 0) {
+    userInput.disabled = true;
+    userInput.hidden = true;
+    remainingTime.textContent = `Net: ${fastFingersGame.currentIndex - fastFingersGame.skippedCount}. Total: ${fastFingersGame.currentIndex}. Skipped: ${fastFingersGame.skippedCount}.`;
+  }
+}, 10);
+
+userInput.addEventListener('input', function(evt) {
+  fastFingersService.startTimeIfNeeded(fastFingersGame); // timer starts when user starts inputting
+  const userInput = evt.target.value;
+  if (evt.data === ' ') {
+    fastFingersService.proceed(fastFingersGame, true);
+    evt.target.value = '';
+    fetchNextImageShowCurrentImage();
+  }
+  if (userInput === fastFingersService.getCurrentWord(fastFingersGame)) {
+    fastFingersService.proceed(fastFingersGame);
+    evt.target.value = '';
+    fetchNextImageShowCurrentImage();
+  }
+  redraw();
+});
+
+function fetchNextImageShowCurrentImage() {
+  let currentIndex = fastFingersGame.currentIndex;
+
+  // this actually fetches next pokemon image
+  const pokemonImg = document.createElement("img");
+  pokemonImg.src = `${baseUrl}${fastFingersService.getNextId(fastFingersGame)}.png`;
+
+  if (currentIndex % 2 === 0) {
+    oddImageDiv.innerHTML = '';
+    oddImageDiv.appendChild(pokemonImg);
+    oddImageDiv.hidden = true;
+    evenImageDiv.hidden = false;
+  } else {
     evenImageDiv.innerHTML = '';
     evenImageDiv.appendChild(pokemonImg);
-
-    userInput.hidden = false;
-    userInput.disabled = false;
-    userInput.value = '';
-    userInput.focus();
-    fetchNextImageShowCurrentImage();
-    redraw();
+    evenImageDiv.hidden = true;
+    oddImageDiv.hidden = false;
   }
+}
 
-  byId("new-game-button").addEventListener('click', newGame);
-
-  setInterval(function() {
-    remainingTime.innerHTML = '';
-    remainingTime.textContent = fastFingersGame.remainingTime;
-    if (fastFingersGame.remainingTime === 0) {
-      userInput.disabled = true;
-      userInput.hidden = true;
-      remainingTime.textContent = `Net: ${fastFingersGame.currentIndex - fastFingersGame.skippedCount}. Total: ${fastFingersGame.currentIndex}. Skipped: ${fastFingersGame.skippedCount}.`;
+function redraw() {
+  currentWord.innerHTML = '';
+  const currentPokemonName = fastFingersService.getCurrentWord(fastFingersGame);
+  const letters = currentPokemonName.split('');
+  for (let i = 0; i < letters.length; i++) {
+    let letterSpan = document.createElement("span");
+    letterSpan.innerText = letters[i];
+    if (userInput.value[i] && userInput.value[i] === letters[i]) {
+      letterSpan.classList.add('correct');
     }
-  }, 10);
-
-  userInput.addEventListener('input', function(evt) {
-    fastFingersService.startTimeIfNeeded(fastFingersGame); // timer starts when user starts inputting
-    const userInput = evt.target.value;
-    if (evt.data === ' ') {
-      fastFingersService.proceed(fastFingersGame, true);
-      evt.target.value = '';
-      fetchNextImageShowCurrentImage();
+    if (userInput.value[i] && userInput.value[i] !== letters[i]) {
+      letterSpan.classList.add('wrong');
     }
-    if (userInput === fastFingersService.getCurrentWord(fastFingersGame)) {
-      fastFingersService.proceed(fastFingersGame);
-      evt.target.value = '';
-      fetchNextImageShowCurrentImage();
-    }
-    redraw();
-  });
-
-  function fetchNextImageShowCurrentImage() {
-    let currentIndex = fastFingersGame.currentIndex;
-
-    // this actually fetches next pokemon image
-    const pokemonImg = document.createElement("img");
-    pokemonImg.src = `${baseUrl}${fastFingersService.getNextId(fastFingersGame)}.png`;
-
-    if (currentIndex % 2 === 0) {
-      oddImageDiv.innerHTML = '';
-      oddImageDiv.appendChild(pokemonImg);
-      oddImageDiv.hidden = true;
-      evenImageDiv.hidden = false;
-    } else {
-      evenImageDiv.innerHTML = '';
-      evenImageDiv.appendChild(pokemonImg);
-      evenImageDiv.hidden = true;
-      oddImageDiv.hidden = false;
-    }
+    currentWord.appendChild(letterSpan);
   }
+  nextWord.textContent = fastFingersService.getNextWord(fastFingersGame);
+}
 
-  function redraw() {
-    currentWord.innerHTML = '';
-    const currentPokemonName = fastFingersService.getCurrentWord(fastFingersGame);
-    const letters = currentPokemonName.split('');
-    for (let i = 0; i < letters.length; i++) {
-      let letterSpan = document.createElement("span");
-      letterSpan.innerText = letters[i];
-      if (userInput.value[i] && userInput.value[i] === letters[i]) {
-        letterSpan.classList.add('correct');
-      }
-      if (userInput.value[i] && userInput.value[i] !== letters[i]) {
-        letterSpan.classList.add('wrong');
-      }
-      currentWord.appendChild(letterSpan);
-    }
-    nextWord.textContent = fastFingersService.getNextWord(fastFingersGame);
-  }
-
-  // Start new game when page loads
-  newGame();
-})();
+// Start new game when page loads
+newGame();
--- src/fastFingersService.js
+++ src/fastFingersService.js
@@ -1,68 +1,72 @@
-import {pokemons} from './pokemons.js';
+import pokemons from './pokemons.js';
 
-export default (function() {
-
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
+}
 
-  function newFastFingersGame() {
-    return {
-      pokemons: shuffleArray(pokemons),
-      currentIndex: 0,
-      skippedCount: 0,
-      remainingTime: 60,
-      timer: undefined,
-      startTimer() {
-        this.timer = setInterval(() => {
-          if (this.remainingTime > 0) {
-            this.remainingTime = this.remainingTime - 1;
-          }
-        }, 1000)
-      },
-    };
-  }
-
-  function getCurrentId(fastFingersGame) {
-    return fastFingersGame.pokemons[fastFingersGame.currentIndex].id;
-  }
-
-  function getNextId(fastFingersGame) {
-    return fastFingersGame.pokemons[fastFingersGame.currentIndex + 1].id;
-  }
-
-  function getCurrentWord(fastFingersGame) {
-    return fastFingersGame.pokemons[fastFingersGame.currentIndex].name;
-  }
-
-  function getNextWord(fastFingersGame) {
-    return fastFingersGame.pokemons[fastFingersGame.currentIndex + 1].name;
-  }
-
-  function proceed(fastFingersGame, skipped = false) {
-    fastFingersGame.currentIndex++;
-    if (skipped) {
-      fastFingersGame.skippedCount++;
-    }
-  }
-
-  function startTimeIfNeeded(fastFingersGame) {
-    if (fastFingersGame.timer === undefined) {
-      fastFingersGame.startTimer();
-    }
-  }
-
+function newFastFingersGame() {
   return {
-    newFastFingersGame,
-    getCurrentWord,
-    getNextWord,
-    getCurrentId,
-    getNextId,
-    proceed,
-    startTimeIfNeeded
+    pokemons: shuffleArray(pokemons),
+    currentIndex: 0,
+    skippedCount: 0,
+    remainingTime: 60,
+    timer: undefined,
+    startTimer() {
+      this.timer = setInterval(() => {
+        if (this.remainingTime > 0) {
+          this.remainingTime = this.remainingTime - 1;
+        }
+      }, 1000)
+    },
+  };
+}
+
+function getCurrentId(fastFingersGame) {
+  return fastFingersGame.pokemons[fastFingersGame.currentIndex].id;
+}
+
+function getNextId(fastFingersGame) {
+  return fastFingersGame.pokemons[fastFingersGame.currentIndex + 1].id;
+}
+
+function getCurrentWord(fastFingersGame) {
+  return fastFingersGame.pokemons[fastFingersGame.currentIndex].name;
+}
+
+function getNextWord(fastFingersGame) {
+  return fastFingersGame.pokemons[fastFingersGame.currentIndex + 1].name;
+}
+
+function proceed(fastFingersGame, skipped = false) {
+  fastFingersGame.currentIndex++;
+  if (skipped) {
+    fastFingersGame.skippedCount++;
   }
-}());
+}
+
+function startTimeIfNeeded(fastFingersGame) {
+  if (fastFingersGame.timer === undefined) {
+    fastFingersGame.startTimer();
+  }
+}
+
+// This actually exports an object
+// if it was exporting in the form of
+// export {newFastFingersGame, getCurrentWord..}
+// then it would have looked like it was exporting
+// an object where in reality it is just exporting
+// a list of separate values.
+// The difference is between `export` and `export default`
+export default {
+  newFastFingersGame,
+  getCurrentWord,
+  getNextWord,
+  getCurrentId,
+  getNextId,
+  proceed,
+  startTimeIfNeeded
+}
